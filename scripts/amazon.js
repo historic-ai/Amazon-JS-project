9071e2ea0fba48bf6a126c153311631528fd90c4
venkey <venkeygandikota143@gmail.com>
2026-04-08 14:33:22 +0530

    make the cart aunatity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -75,6 +75,15 @@ forEach((button)=>{
         quantity:1
        });
     }
+    let cartQuantity=0;
+
+      cart.forEach((item)=>{
+        cartQuantity+=item.quantity;
+       
+      })
+      document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+       console.log(cartQuantity);
+
        console.log(cart);
         });
     
