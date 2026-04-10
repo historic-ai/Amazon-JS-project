87eb2b553f87a1713e90691d80a36a3eef3e0e47
venkey <venkeygandikota143@gmail.com>
2026-04-10 16:27:35 +0530

    creste automated tests for format currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,22 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+console.log("test suite:format Currency");
+console.log("converts cents into dollars");
+if (formatCurrency(2095)==='20.95'){
+    console.log('passed');
+}else {
+    console.log('failed');
+}
+console.log("works with zero");
+if (formatCurrency(0)==='0.00'){
+    console.log('passsed');
+}
+else{
+    console.log('failed');
+}
+console.log('rounds up to neareset cent')
+if (formatCurrency(2000.5)==='20.01'){
+    console.log('passed')
+}
+else{
+    console.log('failed');
+}
